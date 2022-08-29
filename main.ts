4f57a8035758a8ead670a0b074c7877842e9c2b9
PhilippAusHeidelberg <110686148+PhilippAusHeidelberg@users.noreply.github.com>
2022-08-29 20:21:43 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.showString("hi tim", 200)
+basic.showString("ich habe", 200)
+basic.showString("mich sehr", 200)
+basic.showString("uber deine", 200)
+basic.showString("Postkarte", 200)
+basic.showString("gefreut", 200)
+basic.showString("dein Philipp", 200)
